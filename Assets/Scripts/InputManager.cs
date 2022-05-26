5157ce37e1ae7a59542ecbf8b63b0a9994383a03
blowedglass <thenolaconnor@gmail.com>
2022-05-25 22:23:13 -0500

    added player controller script, input, and look. few mesh colliders added

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        // below- permformed, started,canceled.  ctx=> is a reference back to the onfoot jump
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    void FixedUpdate()
+    {
+        //tell playermotor to move using values from movement action.
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+    void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+ 
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
